f4c76c53cfacbfaeb9ffcb3494d961d381245c02
OlegKotkov <tigrastov@gmail.com>
2022-03-28 20:23:48 +0200

    Initial Commit

---
+++ Сеть часть 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Сеть часть 2
+//
+//  Created by mac on 28.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
